30b106faa8a94f5e13cd295147a88998a486aa30
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-04-25 21:43:17 -0300

    sass and css file created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <body>
+        Advice
+        {/* #<!-- Advice ID goes here -->
+
+"<!-- Advice text goes here -->" */}
+
+
+        <div class="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+          Coded by <a href="#">Your Name Here</a>.
+        </div>
+      </body>
     </div>
   );
 }
