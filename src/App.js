35ba3f2800ee26fc15e28eb50812361c90b128d9
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-05-01 19:57:47 -0300

    update app

--- src/App.js
+++ src/App.js
@@ -1,37 +1,68 @@
-// importação do axios
-import axios from "axios";
-
-// instânciação
-const api = axios.create({
-  //Random advice
-  baseURL: 'https://api.adviceslip.com/advice'
-})
+import Api from "./services/api";
+import React, { useEffect, useState } from 'react';
+import { dice, pattern_divider_mobile } from "./components/images";
 
 
 // requisição 
-//https://axios-http.com/ptbr/docs/example
-api.get('').then((Response)=>{
-  console.log(Response)
-})
+// https://axios-http.com/ptbr/docs/example
+// const conselho = advice 
 
+// Id_slip.textContent= JSON.stringify(conselho)
+
+
+
+
+const getAdvice = () => {
+
+  Api.get()
+    .then((response) => {
+      const { slip } = response.data;
+      const { id } = slip;
+      const { advice } = slip;
+      console.log('id: ', id, '\n', 'advice: ', advice)
+
+      const advice_id = document.querySelector('#advice_id');
+      advice_id.textContent = '# ' + id;
+      const advice_message = document.querySelector('#advice_message');
+      advice_message.textContent = '"' + advice + '"';
+
+    })
+    .catch(console.error('error'))
+}
 
 function App() {
   
   return (
     <div className="App">
-      <body>
-        Advice
+    {getAdvice()}
 
-        {/* #<!-- Advice ID goes here -->
-
-"<!-- Advice text goes here -->" */}
-
-
-        <div class="attribution">
-          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-          Coded by <a href="#">Your Name Here</a>.
+      <main className="main">
+        <div className="advice">
+          <div>
+            <span className="advice-title">A D V I C E</span>
+            <span className="advice-id" id="advice_id"></span>
+          </div>
+          <div>
+            <span className="advice-message" id="advice_message"></span>
+          </div>
+          <div>
+            {pattern_divider_mobile}
+          </div>
+          <div>
+            <button onClick={getAdvice} className='advice-button' id="advice_button">
+              {dice}
+            </button>
+          </div>
         </div>
-      </body>
+      </main>
+
+      <footer className="footer">
+        <div class="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a> .
+          <br />
+          Coded by <a href="https://github.com/odisseu93" target={"_blank"}>Ulisses Silvério</a> .
+        </div>
+      </footer>
     </div>
   );
 }
